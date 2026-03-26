c34ec37ee9f6bc692c26a61d2254259bbf53710e
feeqee <freiaquadros16@gmail.com>
2026-03-26 12:13:00 +0530

    Fixed Tailwind setup with correct ESM configuration and PostCSS integration

--- prism-frontend/src/App.jsx
+++ prism-frontend/src/App.jsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <h1 className="text-4xl font-bold text-blue-500">
+    <><h1 className="text-4xl font-bold text-blue-500">
       PRISM is alive 🚀
-    </h1>
+    </h1><h1 className="text-4xl text-green-500 font-bold">
+        Tailwind is Working 🚀
+      </h1></>
   );
 }
 
