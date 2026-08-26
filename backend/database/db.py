4663431513555131e44e9cc7636e4e6a2df31582
thecamouflagedgeek <pretentiousgazette@gmail.com>
2026-08-26 12:44:46 +0530

    Integrated Freia's working end to end

--- backend/database/db.py
+++ backend/database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "postgresql://postgres:prism@localhost:5433/prism_db"
+DATABASE_URL = "postgresql://postgres:sirius*@localhost:5432/prism_db"
 
 engine = create_engine(
     DATABASE_URL,
--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app=FastAPI(title="PRISM Backend")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
+        "http://localhost:5173","http://localhost:5174"
     ],
     allow_credentials=True,
     allow_methods=["*"],
